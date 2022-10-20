11a70feb02f3005be03cfe481181c4202fe3bf3a
Karolina Redden <dubajkaro@gmail.com>
2022-10-20 16:33:13 +0000

    add question 1

---
+++ 1.js
@@ -0,0 +1,10 @@
+// find the two largest numbers in that array, and sum them together
+const sumLargestNumbers = function(data) {
+  // Put your solution here
+};
+
+console.log(sumLargestNumbers([1, 10]));
+console.log(sumLargestNumbers([1, 2, 3]));
+console.log(sumLargestNumbers([10, 4, 34, 6, 92, 2]));
+
+
