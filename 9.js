189d1c423c7d6cd632d56a0a188fcf13a62be888
Karolina Redden <dubajkaro@gmail.com>
2022-12-06 19:07:56 +0000

    add a function which takes care of camelCasing and oes not change the first letter

--- 9.js
+++ 9.js
@@ -3,20 +3,34 @@
 //Create a function named camelCase that will convert a string to camel case, 
 //and return the result.
 
+//
+// const camelCase = function(input) {
+//   const titleCase = input
+//   .split(" ")
+//   .map(word => 
+//     word.charAt(0).toUpperCase() + word.slice(1)
+//   )
+//   .join("")
+//   return titleCase
+// };
 
 const camelCase = function(input) {
-  const titleCase = input
-  .split(" ")
-  .map(word => {
-    return word.charAt(0).toUpperCase() + word.slice(1)
-  })
-  .join("")
-  // let string = input.split(" ")
-  // string.map(word => word.charAt(0).toUpperCase() + word.slice(1))
-  //     // word.join(" ")
-  // return string
-  return titleCase
-};
+  let arr = input.split(" ")
+  let result = " "
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== 0) {
+      result += arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
+    }
+    else {
+      result += arr[i].toLowerCase()
+    }
+  }
+  return result
+}
+
+
+
+
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
