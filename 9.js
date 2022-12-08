30f5d4c96e7696d6a5f9e865f4e81dcce75e4cfb
Karolina Redden <dubajkaro@gmail.com>
2022-12-08 00:42:36 +0000

    format the code

--- 9.js
+++ 9.js
@@ -1,53 +1,47 @@
-// We will receive a normal string of words separated with spaces as the input. 
+// We will receive a normal string of words separated with spaces as the input.
 // Our job is to convert these strings into camel cased strings.
-//Create a function named camelCase that will convert a string to camel case, 
+//Create a function named camelCase that will convert a string to camel case,
 //and return the result.
-
 
 const camelCase = function (input) {
   const titleCase = input
     .split(" ")
     .map((word, i) => {
-      console.log(i)
-      if (i !== 0)
-        return word.charAt(0).toUpperCase() + word.slice(1)
+      console.log(i);
+      if (i !== 0) return word.charAt(0).toUpperCase() + word.slice(1);
       else {
-        return word
+        return word;
       }
-    }
-    )
-    .join("")
-  return titleCase
+    })
+    .join("");
+  return titleCase;
 };
 
 const camelCase2 = function (input) {
-  let arr = input.split(" ")
-  let result = " "
+  let arr = input.split(" ");
+  let result = " ";
   for (let i = 0; i < arr.length; i++) {
     if (i !== 0) {
-      result += arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
-    }
-    else {
-      result += arr[i].toLowerCase()
+      result += arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    } else {
+      result += arr[i].toLowerCase();
     }
   }
-  return result
-}
+  return result;
+};
 
 const camelCase3 = function (input) {
-  let arr = input.split(" ")
-  let result = " "
+  let arr = input.split(" ");
+  let result = " ";
   for (let [i, element] of arr.entries()) {
     if (i !== 0) {
-      result += arr[i].charAt(0).toUpperCase() + arr[i].slice(1)
+      result += arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
     } else {
-      result += arr[i].toLowerCase()
+      result += arr[i].toLowerCase();
     }
   }
-  return result
-}
-
-
+  return result;
+};
 
 console.log(camelCase("this is a string"));
 console.log(camelCase("loopy lighthouse"));
