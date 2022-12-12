481aa8ff1e9583d9b3d73c77b0aaf52634dc0e4c
Karolina Redden <dubajkaro@gmail.com>
2022-12-12 19:22:50 +0000

    sphere volume calculation

---
+++ 11.js
@@ -0,0 +1,50 @@
+const PI = 3.14159 ;
+
+const sphereVolume = function (radius) {
+  console.log(radius)
+  let volume = 4/3*PI*Math.pow(radius, 3)
+  console.log(volume)
+  return volume
+}
+
+console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
+
+const coneVolume = function (radius, height) {
+  // And here!
+}
+
+console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
+
+const prismVolume = function (height, width, depth) {
+  // Probably here too!
+}
+
+console.log(prismVolume(3, 4, 5) === 60);
+
+const totalVolume = function (solids) {
+  // Code here? Yup!
+}
+
+const largeSphere = {
+  type: 'sphere',
+  radius: 40
+}
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10
+}
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5
+}
+
+const duck = [
+  largeSphere,
+  smallSphere,
+  cone
+]
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
