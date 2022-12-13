c5582226707e2acc1cd49317a938dc82901d7ac4
Karolina Redden <dubajkaro@gmail.com>
2022-12-13 15:59:25 +0000

    refactored function to account for solids variable

--- 11.js
+++ 11.js
@@ -1,24 +1,20 @@
-const PI = 3.14159 ;
+const PI = 3.14159;
 
 const sphereVolume = function (radius) {
-  console.log(radius)
-  let volume = 4/3*PI*Math.pow(radius, 3)
-  console.log(volume)
+  let volume = 4 / 3 * PI * Math.pow(radius, 3)
   return volume
 }
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189);
 
 const coneVolume = function (radius, height) {
-  let volume = 1/3*PI*Math.pow(radius, 2)*height;
-  console.log(volume)
+  let volume = 1 / 3 * PI * Math.pow(radius, 2) * height;
   return volume;
 }
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49);
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
   let base = width * depth;
   let volume = height * base;
   return volume;
@@ -27,16 +23,15 @@ const prismVolume = function (height, width, depth) {
 console.log(prismVolume(3, 4, 5) === 60);
 
 const totalVolume = function (solids) {
-  let largeSphereVolume=4/3*PI*Math.pow(largeSphere.radius, 3)
-  console.log("lsv", largeSphereVolume)
-  console.log(smallSphere.radius)
-  let smallSphereVolume=4/3*PI*Math.pow(smallSphere.radius, 3)
-  console.log("ssv", smallSphereVolume)
-  let coneVolume = 1/3*PI*Math.pow(cone.radius, 2)*cone.height;
-  console.log("conevolume", coneVolume)
-  console.log(largeSphereVolume + smallSphereVolume + coneVolume)
-  return largeSphereVolume + smallSphereVolume + coneVolume
-
+  let total = 0;
+  for (let solid of solids) {
+    if (solid.type === 'sphere') {
+      total += sphereVolume(solid.radius)
+    } else if (solid.type === 'cone') {
+      total += coneVolume(solid.radius, solid.height)
+    }
+  }
+  return total
 }
 
 const largeSphere = {
